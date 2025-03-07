a36fbfe43e8bf82f49a5105c73de28fb40de350a
dominickcrosbie <dominick.crosbie@outlook.com>
2025-03-07 15:48:27 +1100

    Update deploy.yml

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueTypedJs from 'vue-typed-js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueTypedJs)
+app.mount('#app')
